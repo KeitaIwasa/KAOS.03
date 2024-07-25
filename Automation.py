0599e518266d41a17f2a3aa7783762990b317006
Keita Iwasa <keita030909@gmail.com>
2024-07-25 11:35:22 +0900

    csv download error handling

--- Automation.py
+++ Automation.py
@@ -104,43 +104,46 @@ class AutomationHandler:
         self.csv_path = f'{self.download_folder_path()}/{today_str_csv}_発注.CSV'
         self.csv_path_nonfood = f'{self.download_folder_path()}/{today_str_csv}_発注 (1).CSV'
         max_retry_download = 15
-        if not os.path.exists(self.csv_path):           
-            # 左メニューの発注照会をクリック
-            WebDriverWait(self.driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, 'menupng2'))).click() #発注
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//a[@accesskey='4']"))).click() #発注照会クリック
+        try:
+            if not os.path.exists(self.csv_path):           
+                # 左メニューの発注照会をクリック
+                WebDriverWait(self.driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, 'menupng2'))).click() #発注
+                WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//a[@accesskey='4']"))).click() #発注照会クリック
 
-            # 本日の発注を照会
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, 'inquiryButton'))).click() #照会ボタン
+                # 本日の発注を照会
+                WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, 'inquiryButton'))).click() #照会ボタン
 
-            # 画面の一番下までスクロール
-            self.driver.execute_script("document.body.style.zoom='65%'")
-            self.driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+                # 画面の一番下までスクロール
+                self.driver.execute_script("document.body.style.zoom='65%'")
+                self.driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+                    
+                # CSVをダウンロード
+                btn_csv_out = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, 'btnCsvoutConfirm')))
+                self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", btn_csv_out)
+                time.sleep(0.3)
+                self.driver.execute_script("arguments[0].click();", btn_csv_out)  # JavaScriptでクリックを強制実行
+                # WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, 'btnCsvoutConfirm'))).click() #CSV出力をクリック
+                # WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[text()='はい']"))).click() 
                 
-            # CSVをダウンロード
-            btn_csv_out = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, 'btnCsvoutConfirm')))
-            self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", btn_csv_out)
-            time.sleep(0.3)
-            self.driver.execute_script("arguments[0].click();", btn_csv_out)  # JavaScriptでクリックを強制実行
-            # WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, 'btnCsvoutConfirm'))).click() #CSV出力をクリック
-            # WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[text()='はい']"))).click() 
-            
-            # ウィジェットのはいをクリック
-            btn_yes = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[text()='はい']")))
-            self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", btn_yes)
-            time.sleep(0.3)  # 少し待機
-            self.driver.execute_script("arguments[0].click();", btn_yes)
+                # ウィジェットのはいをクリック
+                btn_yes = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[text()='はい']")))
+                self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", btn_yes)
+                time.sleep(0.3)  # 少し待機
+                self.driver.execute_script("arguments[0].click();", btn_yes)
 
-            today_str_csv = datetime.today().strftime('%Y%m%d') #本日の日付（ダウンロードした発注明細csvは発注日付に関係なく本日のreal日付が付いている）
-            retry_download = 0
-            # 前日の発注明細をロード
-            while retry_download <= max_retry_download:
-                if os.path.exists(self.csv_path):
-                    break
-                elif retry_download == max_retry_download:
-                    return False    
-                else:
-                    retry_download += 1
-                    time.sleep(0.1)
+                today_str_csv = datetime.today().strftime('%Y%m%d') #本日の日付（ダウンロードした発注明細csvは発注日付に関係なく本日のreal日付が付いている）
+                retry_download = 0
+                # 前日の発注明細をロード
+                while retry_download <= max_retry_download:
+                    if os.path.exists(self.csv_path):
+                        break
+                    elif retry_download == max_retry_download:
+                        return False    
+                    else:
+                        retry_download += 1
+                        time.sleep(0.1)
+        except:
+            return False
 
         if today_int.weekday() in {3, 5}:#木夜、土夜の場合は非食品の発注明細も取得・合成
             if os.path.exists(self.csv_path_nonfood):
